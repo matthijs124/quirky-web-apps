fbc22475fc628103196204b9856b0537f08ca22a
matthijs124 <matthijsdoolaar@live.nl>
2020-01-09 00:00:56 +0100

    added constraints for stack

--- stacker/app.js
+++ stacker/app.js
@@ -6,17 +6,25 @@ var main = function () {
 
     $("#add").on("click", function (event) {
         if (count < 5) {
+            $("#alertbox").empty();
             count++;
             console.log(count);
             $("#rightdiv").prepend("<img src='delftbox.png'>");
+        } else {
+            $("#alertbox").empty();
+            $("#alertbox").append("<p>Stack is full!</p>");
         }
     });
 
     $("#remove").on("click", function (event) {
         if (count > 0) {
+            $("#alertbox").empty();
             count--;
             console.log(count);
             $('#rightdiv').find("img").slice(count).remove();
+        } else {
+            $("#alertbox").empty();
+            $("#alertbox").append("<p>Nothing left to remove!</p>");
         }
     });
 };
