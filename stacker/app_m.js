6863cc6cf94faf220e485657b69d0c72c7e61cdb
matthijs124 <matthijsdoolaar@live.nl>
2020-01-09 23:20:59 +0100

    implement winning into actual app (w/o visuals)

--- stacker/app_m.js
+++ stacker/app_m.js
@@ -19,7 +19,12 @@ var main = function () {
                 } else {
                     $("#turnbox").empty();
                 }
-    
+                
+                updateboard(index, 1);
+                if (checkwin(board, 1, index)) {
+                    console.log("BLUE HAS WON ---------")
+                }
+
             } else {
                 $("#"+ (index+1) + "_" + stacks[index]).append("<img src='delftboxred.png'>");
                 $(this).css("background-color", "rgb(0, 166, 214)");
@@ -30,6 +35,11 @@ var main = function () {
                     $("#turnbox").append("<span><p class='turn-p'>Blue's turn</p></span>");
                 } else {
                     $("#turnbox").empty();
+                }
+
+                updateboard(index, 2);
+                if (checkwin(board, 2, index)) {
+                    console.log("RED HAS WON ---------")
                 }
             }
             totalBoxes++;
@@ -59,7 +69,114 @@ var main = function () {
         $("#turnbox").empty();
         totalBoxes = 0;
         stacks = [0, 0, 0, 0, 0, 0, 0];
+        board = [
+            [9,9,9,9,9,9,9,9,9,9,9,9,9],
+            [9,9,9,9,9,9,9,9,9,9,9,9,9],
+            [9,9,9,9,9,9,9,9,9,9,9,9,9],
+            [9,9,9,0,0,0,0,0,0,0,9,9,9],
+            [9,9,9,0,0,0,0,0,0,0,9,9,9],
+            [9,9,9,0,0,0,0,0,0,0,9,9,9],
+            [9,9,9,0,0,0,0,0,0,0,9,9,9],
+            [9,9,9,0,0,0,0,0,0,0,9,9,9],
+            [9,9,9,0,0,0,0,0,0,0,9,9,9],
+            [9,9,9,0,0,0,0,0,0,0,9,9,9],
+            [9,9,9,9,9,9,9,9,9,9,9,9,9],
+            [9,9,9,9,9,9,9,9,9,9,9,9,9],
+            [9,9,9,9,9,9,9,9,9,9,9,9,9]];
     });
+
+    var board = [
+    [9,9,9,9,9,9,9,9,9,9,9,9,9],
+    [9,9,9,9,9,9,9,9,9,9,9,9,9],
+    [9,9,9,9,9,9,9,9,9,9,9,9,9],
+    [9,9,9,0,0,0,0,0,0,0,9,9,9],
+    [9,9,9,0,0,0,0,0,0,0,9,9,9],
+    [9,9,9,0,0,0,0,0,0,0,9,9,9],
+    [9,9,9,0,0,0,0,0,0,0,9,9,9],
+    [9,9,9,0,0,0,0,0,0,0,9,9,9],
+    [9,9,9,0,0,0,0,0,0,0,9,9,9],
+    [9,9,9,0,0,0,0,0,0,0,9,9,9],
+    [9,9,9,9,9,9,9,9,9,9,9,9,9],
+    [9,9,9,9,9,9,9,9,9,9,9,9,9],
+    [9,9,9,9,9,9,9,9,9,9,9,9,9]];
+
+    var updateboard = function(input, player) {
+        for(var i=board[input+3].length-3; i>2; i--) {
+            if(board[input+3][i] === 0) {
+                board[input+3][i] = player;
+                break;
+            }
+        }
+    }
+
+    var checkwin = function(board, player, input) {
+
+        var adjusted = input + 3;
+        var string="";
+        var index;
+
+    for(var i=0; i<board[adjusted].length; i++) {
+        if(board[adjusted][i] === player) {
+            index=i;
+            break;
+        }
+    }
+
+    // add the vertical and horizontal
+    string += "" +
+        board[adjusted][index-3] +
+        board[adjusted][index-2] +
+        board[adjusted][index-1] +
+        board[adjusted][index] +
+        board[adjusted][index+1] +
+        board[adjusted][index+2] +
+        board[adjusted][index+3] +
+        "8" +
+        board[adjusted-3][index] +
+        board[adjusted-2][index] +
+        board[adjusted-1][index] +
+        board[adjusted][index] +
+        board[adjusted+1][index] +
+        board[adjusted+2][index] +
+        board[adjusted+3][index];
+
+    // add the diagonals
+    string += "8" +
+        board[adjusted-3][index-3] +
+        board[adjusted-2][index-2] +
+        board[adjusted-1][index-1] +
+        board[adjusted][index] +
+        board[adjusted+1][index+1] +
+        board[adjusted+2][index+2] +
+        board[adjusted+3][index+3] +
+        "8" +
+        board[adjusted-3][index+3] + 
+        board[adjusted-2][index+2] +
+        board[adjusted-1][index+1] +
+        board[adjusted][index] +
+        board[adjusted+1][index-1] +
+        board[adjusted+2][index-2] +
+        board[adjusted+3][index-3];
+
+    var substring = ""+player+player+player+player;
+
+    console.log("string: " + string);
+    console.log("sub: " + substring);
+
+    return string.includes(substring);
+    }
+
+    var printboard = function(board) {
+        var string = "";
+        for(var i=0; i<board.length; i++){
+            string += " | ";
+            for(var j=0; j<board[i].length; j++) {
+                string += board[i][j] + " | ";
+            }
+            string += "\n";
+        }
+        console.log(string);
+    }
 };
 
 $(document).ready(main);
