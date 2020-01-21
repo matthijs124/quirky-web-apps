e3b550f40cf256c50185ca3f56e8123f43ba5b75
matthijs124 <matthijsdoolaar@live.nl>
2020-01-21 09:00:32 +0100

    added timer and some visual changes

--- stacker/app_m.js
+++ stacker/app_m.js
@@ -92,7 +92,7 @@ var main = function () {
             [9,9,9,9,9,9,9,9,9,9,9,9,9],
             [9,9,9,9,9,9,9,9,9,9,9,9,9],
             [9,9,9,9,9,9,9,9,9,9,9,9,9]];
-        
+        sec = 0;
         $("body").css("background-color", "lightsteelblue");
     });
 
@@ -188,6 +188,14 @@ var main = function () {
         }
         console.log(string);
     }
+
+    //timer
+    var sec = 0;
+    function pad ( val ) { return val > 9 ? val : "0" + val; }
+    setInterval( function(){
+        document.getElementById("seconds").innerHTML=pad(++sec%60);
+        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+    }, 1000);
 };
 
 $(document).ready(main);
